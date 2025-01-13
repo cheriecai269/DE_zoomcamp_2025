d95290a257198617acee3e16fcc3731030a7b55d
Cherie <xcai269@gmail.com>
2025-01-12 18:43:30 -0800

    Add initial Docker setup with Python script for data processing

---
+++ 01-docker-terraform/02_docker_sql/pipeline.py
@@ -0,0 +1,9 @@
+# Import libraries
+
+import pandas as pd
+import sys
+
+print('Input from commandline: ', sys.argv)
+print('What date do we want to alter the data? Answer: ', sys.argv[1])
+print('Job finished successfully!')
+
